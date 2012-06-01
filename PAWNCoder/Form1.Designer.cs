6816c1f47f2c80b16a03da2787dcb2350b6602bd
Bubelbub <bubelbub@googlemail.com>
2012-06-01 16:51:44 +0200

    Initial 0.1.2

---
+++ PAWNCoder/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PAWNCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Location = new System.Drawing.Point(643, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(5, 334);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(545, 297);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "PAWNCode generieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(629, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(141, 113);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(629, 39);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(141, 22);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(556, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(820, 660);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(470, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 9);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Copyright © RG  2012";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(5, 7);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(545, 306);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.Location = new System.Drawing.Point(646, 133);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(95, 26);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DIALOG_STYLE_LIST",
+            "DIALOG_STYLE_MSGBOX",
+            "DIALOG_STYLE_INPUT",
+            "DIALOG_STYLE_PASSWORD"});
+            this.comboBox1.Location = new System.Drawing.Point(278, 314);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "DialogArt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 643);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "SAMP GUI Designer 0.1.2 ALPHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+    }
+}
+
---
+++ PAWNCoder/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace PAWNCoder
+{
+    public partial class Form1 : Form
+    {
+        private const int EM_SETTABSTOPS = 0x00CB;
+
+        [DllImport("User32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int[] lParam);
+        bool check = false;
+        public Form1()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 750;
+            timer.Tick += new EventHandler(timerevent);
+            timer.Start();
+            InitializeComponent();
+            textBox1.Text = "Dein Dialogcode hier";
+            label2.Text = textBox1.Text.Length.ToString() + " Zeichen";
+            var pos = this.PointToScreen(label1.Location);
+            pos = pictureBox2.PointToClient(pos);
+            label1.Parent = pictureBox2;
+            label1.Location = pos;
+            label1.BackColor = Color.Transparent;
+
+            pos = this.PointToScreen(pictureBox2.Location);
+            pos = pictureBox1.PointToClient(pos);
+            pictureBox2.Parent = pictureBox1;
+            pictureBox2.Location = pos;
+            pictureBox2.BackColor = Color.Transparent;
+
+            this.textBox1.KeyDown += new KeyEventHandler(textBox1_KeyDown);
+            this.textBox2.KeyDown += new KeyEventHandler(textBox2_KeyDown);
+            int[] tabstops = new int[] { 26 };
+            SendMessage(textBox1.Handle, EM_SETTABSTOPS, tabstops.Length, tabstops);
+            textBox1.Invalidate();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            
+        }
+        private void timerevent(Object e, EventArgs unused)
+        {
+            if (!check)
+            {
+                label1.Text = textBox1.Text;
+                label1.Text = Regex.Replace(label1.Text,"\t", "             ");
+                Size test = new Size(label1.Width + 30, label1.Height + 60);
+                if (test.Width < 180) test.Width = 180;
+                pictureBox2.Size = test;
+                test.Height = 21;
+                pictureBox3.Size = test;
+                Point p = new Point(pictureBox1.Location.X + pictureBox2.Location.X + pictureBox2.Width/2-50,
+                    pictureBox1.Location.Y + pictureBox2.Location.Y + pictureBox2.Height - 35);
+                pictureBox4.Location = p;
+                string[] lines = Regex.Split(label1.Text, "\r\n");
+            }
+            check = false;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //label1.Text = textBox1.Text;
+            int index = label1.Text.IndexOf('\t');
+            label2.Text = textBox1.Text.Length.ToString() + " Zeichen";
+        }
+        private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            check = true;
+            if (e.Control && e.KeyValue == 65)
+                textBox1.SelectAll();
+            /*if (e.KeyValue == 9)
+            {
+                
+                string[] lines = Regex.Split(label1.Text, "\r\n");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Length < 1) continue;
+                    for (int u = 0; u < lines[i].Length; u++)
+                    {
+                        if (lines[i][u] == '\t')
+                        {
+                            label2.Text = lines[i].Remove(u, 0);
+                            string blubb = "";
+                            for (int a = u; a < lines[i].Length; a++)
+                            {
+                                blubb = blubb + " ";
+                            }
+                            lines[i] = lines[i].Insert(u, blubb);
+                        }
+                    }
+                }
+                //label1.Text = label1.Text.Replace('\t'.ToString(),blubb );
+            }*/
+        }
+        private void textBox2_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.Control && e.KeyValue == 65)
+                textBox2.SelectAll();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string blubb = textBox1.Text.Replace('\t'.ToString(), ";");
+            string[] lines = Regex.Split(textBox1.Text, "\r\n");
+            lines[0] = lines[0].Replace('\t'.ToString(), "\\t");
+            lines[0] = Regex.Replace(lines[0], '"'.ToString(), "\\\"");
+            lines[0] = Regex.Replace(lines[0], '"'.ToString(), "\\\"");
+            lines[0] = Regex.Replace(lines[0], '%'.ToString(), "%%");
+            textBox2.Text = "new str[" + textBox1.Text.Length + "];\r\nformat(str,sizeof str,\"" + lines[0] + ""+ ( ( lines.Length > 1 ) ? "\\n" : "" )+"\");";
+            for (int i = 1; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Replace('\t'.ToString(), "\\t");
+                lines[i] = Regex.Replace(lines[i], '"'.ToString(), "\\\"");
+                lines[i] = Regex.Replace(lines[i], '%'.ToString(), "%%");
+                if (lines[i].Length < 1 && i != lines.Length -1) lines[i+1] = "\\n"+lines[i+1];
+                else if(lines[i].Length < 1 && i == lines.Length - 1) continue;
+                else
+                    textBox2.Text = textBox2.Text + "\r\nformat(str,sizeof str,\"%s" + lines[i] + "" + ((i == lines.Length - 1) ? "" : "\\n") + "\",str);";
+            }
+            textBox2.Text = textBox2.Text + "\r\nShowPlayerDialog(playerid,dialogid,"+ comboBox1.Text +",\"Headline\",str,\"Ok!\",\"\");";
+
+            
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
